c95c64011661744707428a29664d0689edf42478
Xavier Huerta <kirbyxavier2@outlook.com>
2023-10-03 21:42:25 -0600

    Empezando pruebas 3er Grado

--- BuscaRaices/main.cpp
+++ BuscaRaices/main.cpp
@@ -8,27 +8,6 @@ using namespace std;
 //vector usado para almacenar las raices solucion
 vector <float> raices;
 
-//Aplicamos regla de Ruffini
-vector <float> tercer_G(char const *argv[]){
-    pow(2,2);
-
-    return raices;
-}
-
-float divSintetic(float a, float b, float c, float d){
-    float aux1, aux2, aux3;
-    vector <float> posRootSolve = GenPosRootSolves(a, d);
-    float sumas [4];
-    
-
-    for (int i = 0; i < posRootSolve.size(); i++)
-    {
-        /* code */
-    }
-    
-    return 1;
-}
-
 //Genera un vector con los divisores exactos de d
 vector <float> GenDivsD(float d){
     vector <float> divsD;
@@ -36,21 +15,22 @@ vector <float> GenDivsD(float d){
     for (int i = 1; i < d; i++)
     {
         if(((int) d  % i) == 0){
-            divsD.push_back(i);
-            divsD.push_back(-i);
+            divsD.push_back((float)i);
+            divsD.push_back((float)-i);
         }
     }
     
     return divsD;
 }
+
 //Genera un vector con los divisores exactos de a
 vector <float> GenDivsA(float a){
     vector <float> divsA;
     for (int i = 1; i < a; i++)
     {
         if(((int) a  % i) == 0){
-            divsA.push_back(i);
-            divsA.push_back(-i);
+            divsA.push_back((float)i);
+            divsA.push_back((float)-i);
         }
     }
     
@@ -63,8 +43,6 @@ vector <float> GenPosRootSolves(float a, float d){
     vector <float> dA = GenDivsA(a);
     vector <float> dD = GenDivsD(d);
 
-    int i = 0;
-
     for (int vD = 0; vD < dD.size(); vD++)
     {
         for (int vA = 0; vA < dA.size(); vA++)
@@ -72,11 +50,46 @@ vector <float> GenPosRootSolves(float a, float d){
             posRootSolvesV.push_back(dD.at(vD) / dA.at(vA));
             posRootSolvesV.push_back( -(dD.at(vD) / dA.at(vA)));
         }
-        
     }
     
     return posRootSolvesV;
 }
+//ejecuta una division sintetica hasta encontrar las RootSolves
+float divSintetic(float a, float b, float c, float d){
+    float aux1, aux2, aux3;
+    vector <float> posRootSolve = GenPosRootSolves(a, d);
+    float sumas [4];
+
+    cout << posRootSolve.size();
+
+    for (int i = 0; i < posRootSolve.size(); i++)
+    {
+        cout << " - " << posRootSolve.at(i);
+    }
+
+    // for (int i = 0; i < posRootSolve.size(); i++)
+    // {
+        
+    // }
+    
+    return 1;
+}
+
+
+//Aplicamos regla de Ruffini
+vector <float> tercer_G(char const *argv[]){
+    float a = stof(argv[1]);
+    float b = stof(argv[2]);
+    float c = stof(argv[3]);
+    float d = stof(argv[4]);
+
+    int x = divSintetic(a, b, c, d);
+
+    return raices;
+}
+
+
+
 
 //Aplicamos formula general
 vector <float> segundo_G(char const *argv[]){
@@ -123,10 +136,10 @@ int main(int argc, char const *argv[])
         perror("No igreso correctamente los argumentos");
     }
     else{
-        // if(*argv[1] > 0){//ecuacion de 3er grado
-        //     cout << "Tenemos una Ecuacion de 3er grado" << endl;
-        //     cout << *argv[1] << endl;
-        // }else 
+        if(*argv[1] > 0){//ecuacion de 3er grado
+            cout << "Tenemos una Ecuacion de 3er grado" << endl;
+            tercer_G(argv);
+        }else 
         if(*argv[2] != 0){//ecuacion de 2do grado
 
             cout << "Tenemos una Ecuacion de 2do grado" << endl;
