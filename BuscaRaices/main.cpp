bd22d303afe7d4b728d2bc29b737c557f9cbeb8f
Xavier Huerta <kirbyxavier2@outlook.com>
2023-10-03 21:23:14 -0600

    Configuracion de CMakeLists.txt completa

--- BuscaRaices/main.cpp
+++ BuscaRaices/main.cpp
@@ -1,35 +1,141 @@
 #include <iostream>
-
-#include <Ecuacion.h>
+#include <string>
+#include <math.h>
+#include <vector>
 
 using namespace std;
 
+//vector usado para almacenar las raices solucion
+vector <float> raices;
+
+//Aplicamos regla de Ruffini
+vector <float> tercer_G(char const *argv[]){
+    pow(2,2);
+
+    return raices;
+}
+
+float divSintetic(float a, float b, float c, float d){
+    float aux1, aux2, aux3;
+    vector <float> posRootSolve = GenPosRootSolves(a, d);
+    float sumas [4];
+    
+
+    for (int i = 0; i < posRootSolve.size(); i++)
+    {
+        /* code */
+    }
+    
+    return 1;
+}
+
+//Genera un vector con los divisores exactos de d
+vector <float> GenDivsD(float d){
+    vector <float> divsD;
+
+    for (int i = 1; i < d; i++)
+    {
+        if(((int) d  % i) == 0){
+            divsD.push_back(i);
+            divsD.push_back(-i);
+        }
+    }
+    
+    return divsD;
+}
+//Genera un vector con los divisores exactos de a
+vector <float> GenDivsA(float a){
+    vector <float> divsA;
+    for (int i = 1; i < a; i++)
+    {
+        if(((int) a  % i) == 0){
+            divsA.push_back(i);
+            divsA.push_back(-i);
+        }
+    }
+    
+    return divsA;
+}
+
+//Genera un vector son las posibles raices solucion
+vector <float> GenPosRootSolves(float a, float d){
+    vector <float> posRootSolvesV;
+    vector <float> dA = GenDivsA(a);
+    vector <float> dD = GenDivsD(d);
+
+    int i = 0;
+
+    for (int vD = 0; vD < dD.size(); vD++)
+    {
+        for (int vA = 0; vA < dA.size(); vA++)
+        {
+            posRootSolvesV.push_back(dD.at(vD) / dA.at(vA));
+            posRootSolvesV.push_back( -(dD.at(vD) / dA.at(vA)));
+        }
+        
+    }
+    
+    return posRootSolvesV;
+}
+
+//Aplicamos formula general
+vector <float> segundo_G(char const *argv[]){
+    float a = stof(argv[2]);
+    float b = stof(argv[3]);
+    float c = stof(argv[4]);
+
+    float discriminante = (float) (pow(b,2) - (4 * a * c));
+
+    if(discriminante >= 0){
+        raices.push_back( ( -b + sqrt(discriminante) ) / (2 * a));
+        raices.push_back( ( -b - sqrt(discriminante) ) / (2 * a));
+    }
+
+    return raices;
+}
+
+vector <float> primer_G(char const *argv[]){
+    
+
+    return raices;
+}
+
+void mostrarRaices(vector <float> raices){
+    if(!raices.empty()){
+        cout << "Las raices son: " << endl;
+        cout << "{";
+        for (int i = 0; i < raices.size(); i++)
+        {
+            cout << raices.at(i) << ", ";
+        }
+        cout << "}" << endl;
+    }
+    else{
+        cout << "{}";
+    }
+}
+
+//Metodo Main
 int main(int argc, char const *argv[])
 {
-    //si no ingreso los argumentos enla ejecucion
+    //si no ingreso los argumentos en la ejecucion
     if(argc == 0 || argc < 4){
         perror("No igreso correctamente los argumentos");
     }
     else{
-        if(*argv[1] != 0){//ecuacion de 3er grado
-            
-        }else if(*argv[2] != 0){//ecuacion de 2do grado
-            mostrarRaices(Ecuacion::segundo_G(argv));
-        }else{//ecuacion de tercer grado
+        // if(*argv[1] > 0){//ecuacion de 3er grado
+        //     cout << "Tenemos una Ecuacion de 3er grado" << endl;
+        //     cout << *argv[1] << endl;
+        // }else 
+        if(*argv[2] != 0){//ecuacion de 2do grado
 
+            cout << "Tenemos una Ecuacion de 2do grado" << endl;
+            mostrarRaices(segundo_G(argv));
+
+        }else{//ecuacion de primer grado
+            cout << "Tenemos una Ecuacion de 1er grado" << endl;
         }
     }
 
     return 0;
-}
-
-void mostrarRaices(vector <float> raices){
-    cout << "Las raices son: " << endl;
-    cout << "{";
-    for (int i = 0; i < raices.size(); i++)
-    {
-        cout << raices.at(i) << ", ";
-    }
-    cout << "}" << endl;
-    
 }
